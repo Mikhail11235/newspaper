cd5669b097f87142e0ae98cdab9253cc719c4649
mikhail11235 <m-posevin@mail.ru>
2021-05-02 22:48:43 +0300

    bootstrap

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    
+    'crispy_forms',
+
+    'users',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
