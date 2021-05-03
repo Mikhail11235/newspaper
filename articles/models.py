cd757dbc839066775ba38e85b24592b3aa0d8daf
mikhail11235 <m-posevin@mail.ru>
2021-05-03 16:49:55 +0300

    comments

--- articles/models.py
+++ articles/models.py
@@ -11,8 +11,6 @@ class Article(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    context_object_name = 'aticle'
-
 
     def __str__(self):
         return self.title
@@ -20,3 +18,20 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.comment
+
+
+    def get_absolute_url(self):
+        return reverse('article_list')
